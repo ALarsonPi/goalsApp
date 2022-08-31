dcd795441e0469348111d766008e69643f847701
Alec Larson <ALarsonPi@gmail.com>
2022-08-30 20:59:06 -0600

    Remove final firestore remnants and uploaded first draft to ios app store

--- goals_app/lib/main.dart
+++ goals_app/lib/main.dart
@@ -1,13 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'AppRouter.dart';
 
 Future<void> main() async {
-  //Firebase initialization
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-
   //System Colors initialization
   SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
     systemNavigationBarColor: Colors.transparent, // navigation bar color
