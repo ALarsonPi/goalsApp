13cd5e4bc234afdacccc0fef88c7c5eaad92a109
Alec Larson <ALarsonPi@gmail.com>
2022-12-15 12:45:35 -0700

    Reverted all the priority changes. Apparently something changed there. I'm ok not having that. Changing the massive structure of an already written program is often difficult and sometimes not worth the work it would take. No wonder so many people and companies don't update legacy code - the potential for errors is massive and the process is difficult

--- goals_app/lib/Widgets/Priorities/priorityCard.dart
+++ goals_app/lib/Widgets/Priorities/priorityCard.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:goals_app/Screens/ArgumentPassThroughScreens/individualPriorityArgumentScreen.dart';
+import 'package:goals_app/Widgets/Priorities/roundedCard.dart';
 
 import '../../Screens/Priorities/individualPriority.dart';
 import '../../global.dart';
@@ -44,91 +45,10 @@ class PriorityCard extends StatelessWidget {
           arguments: IndividualPriorityArgumentScreen(index),
         ),
       },
-      child: SizedBox(
-        child: Stack(
-          children: [
-            Padding(
-              padding: EdgeInsets.only(
-                  bottom: !Global.isPhone
-                      ? (MediaQuery.of(context).size.height * 0.1)
-                      : (MediaQuery.of(context).size.height * 0.1)),
-              child: Row(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.735,
-                    child: FittedBox(
-                      fit: BoxFit.fill,
-                      child: ClipRRect(
-                        borderRadius: const BorderRadius.only(
-                          topLeft: Radius.circular(20),
-                          topRight: Radius.circular(20),
-                        ),
-                        child: Align(
-                          alignment: const Alignment(0.0, -0.6),
-                          widthFactor: 1.0,
-                          heightFactor: 1.0,
-                          child: currentImage,
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              bottom: 6.0,
-              right: 0.0,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SizedBox(
-                    width: MediaQuery.of(context).size.width * 0.77,
-                    height: !Global.isPhone
-                        ? MediaQuery.of(context).size.height * 0.12
-                        : MediaQuery.of(context).size.height * 0.10,
-                    child: ClipRRect(
-                      borderRadius: const BorderRadius.only(
-                          bottomLeft: Radius.circular(20),
-                          bottomRight: Radius.circular(20)),
-                      child: Card(
-                        borderOnForeground: false,
-                        elevation: 5,
-                        child: Padding(
-                          padding: EdgeInsets.only(
-                            top: !Global.isPhone ? 12 : 0,
-                            left: !Global.isPhone ? 24 : 0,
-                          ),
-                          child: ListTile(
-                            title: AutoSizeText(
-                              name,
-                              style: TextStyle(
-                                fontSize: !Global.isPhone ? 36 : 18,
-                                fontWeight: FontWeight.bold,
-                              ),
-                            ),
-                            subtitle: AutoSizeText(
-                              "Priority ${index + 1}",
-                              style: TextStyle(
-                                fontSize: !Global.isPhone ? 24 : 12,
-                                fontWeight: FontWeight.normal,
-                                color: Theme.of(context)
-                                    .listTileTheme
-                                    .textColor
-                                    ?.withOpacity(0.5),
-                              ),
-                            ),
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
+      child: RoundedCard(
+        currImage: currentImage,
+        name: name,
+        index: index,
       ),
     );
   }
---
+++ goals_app/lib/Widgets/Priorities/roundedCard.dart
@@ -0,0 +1,95 @@
+import 'package:flutter/material.dart';
+import '../../global.dart';
+
+class RoundedCard extends StatelessWidget {
+  RoundedCard({
+    super.key,
+    required this.currImage,
+    required this.name,
+    required this.index,
+    this.isSmall = false,
+  });
+
+  Widget currImage;
+  String name;
+  int index;
+  bool isSmall;
+
+  @override
+  Widget build(BuildContext context) {
+    return LayoutBuilder(
+      builder: (BuildContext context, BoxConstraints constraints) {
+        return Stack(
+          children: [
+            Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+              elevation: 5,
+              child: ListView(
+                shrinkWrap: false,
+                physics: const NeverScrollableScrollPhysics(),
+                children: [
+                  SizedBox(
+                    width: constraints.maxWidth,
+                    height: (isSmall)
+                        ? constraints.constrainHeight() * 0.7
+                        : constraints.constrainHeight() * 0.75,
+                    child: currImage,
+                  ),
+                ],
+              ),
+            ),
+            Positioned(
+              height: constraints.constrainHeight() - ((isSmall) ? 10 : 20),
+              width: constraints.constrainWidth(),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(bottom: (isSmall) ? 0 : 8.0),
+                    child: Text(
+                      name,
+                      style: TextStyle(
+                        fontSize: !Global.isPhone
+                            ? (isSmall)
+                                ? 30
+                                : 36
+                            : (isSmall)
+                                ? 12
+                                : 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(bottom: 8.0),
+                    child: Text(
+                      "Priority ${index + 1}",
+                      style: TextStyle(
+                        fontSize: !Global.isPhone
+                            ? (isSmall)
+                                ? 18
+                                : 24
+                            : (isSmall)
+                                ? 8
+                                : 12,
+                        fontWeight: FontWeight.normal,
+                        color: Theme.of(context)
+                            .listTileTheme
+                            .textColor
+                            ?.withOpacity(0.5),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
